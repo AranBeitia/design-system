ab7bbb68df07e49158db1e2fb69542973ef45110
Arantza Beitia <abeitia@theinit.com>
2019-03-12 18:22:35 +0100

    Gulp-sass configuration

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # design-system
 Design system
+
+# INSTALATION
+# The basics
+```sh
+$ npm install --save-dev gulp
+$ npm install --save-dev gulp-sass
+```
+
+# Add sourcemaps, autoprefixer and css minification
+```sh
+$ npm install --save-dev gulp-postcss autoprefixer cssnano gulp-sourcemaps
+```
+
+# Inject CSS on compilation and refresh on html change
+```sh
+$ npm install --save-dev browser-sync
+```
+
+# GO
+```sh
+$ gulp watch
+```
+
+# URL details
+https://goede.site/setting-up-gulp-4-for-automatic-sass-compilation-and-css-injection
+
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		postcss = require("gulp-postcss")
+		autoprefixer = require("autoprefixer"),
+		cssnano = require("cssnano"),
+		sourcemaps = require("gulp-sourcemaps"),
+		browserSync = require("browser-sync").create();
+
+// Put this after including our dependencies
+var paths = {
+	styles: {
+		// By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+		src: "assets/sass/*.scss",
+		// Compiled files will end up in whichever folder it's found in (partials are not compiled)
+		dest: "assets/css"
+	}
+	// Easily add additional paths
+	// ,html: {
+	//  src: '...',
+	//  dest: '...'
+	// }
+};
+
+// Define tasks after requiring dependencies
+function style() {
+	return gulp
+		.src(paths.styles.src)
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.on("error", sass.logError)
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.styles.dest))
+		// Add browsersync stream pipe after compilation
+		.pipe(browserSync.stream());
+}
+exports.style = style;
+
+function reload() {
+	browserSync.reload();
+}
+
+function watch() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	gulp.watch(paths.styles.src, style);
+	gulp.watch("index.html", reload);
+}
+// Don't forget to expose the task!
+exports.watch = watch;
