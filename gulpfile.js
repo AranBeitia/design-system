f42c7a3e5fc0392419c74d4f775632e685265dcc
Arantza Beitia <abeitia@theinit.com>
2019-03-14 18:45:09 +0100

    Colour section styles

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var gulp = require('gulp'),
 var paths = {
 	styles: {
 		// By using styles/**/*.sass we're telling gulp to check all folders for any sass file
-		src: "assets/sass/*.scss",
+		src: "assets/sass/**/*.scss",
 		// Compiled files will end up in whichever folder it's found in (partials are not compiled)
 		dest: "assets/css"
 	}
