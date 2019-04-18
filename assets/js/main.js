3336a8fa8d5d11eece8a02cfb597a93329a4efc9
Arantza Beitia <abeitia@theinit.com>
2019-04-18 16:04:39 +0200

    Color layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -192,9 +192,16 @@
 			var accordion = $(this);
 			//detect change in the input[type="checkbox"] value
 			accordion.on('change', 'input[type="checkbox"]', function(){
+        
 				var checkbox = $(this);
 				console.log(checkbox.prop('checked'));
-				( checkbox.prop('checked') ) ? checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300) : checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
+				( checkbox.prop('checked') ) ? checkbox.siblings('ul')
+        .attr('style', 'display:none;')
+        .slideDown(300) : checkbox.siblings('ul')
+        .attr('style', 'display:block;')
+        .slideUp(300);
+
+        $('.accordion__label').toggleClass('accordion__label--open');
 			});
 		});
 	}
